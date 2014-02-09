94c48f7883f86352b90e478ffc25ec0e62ca89ee
Jamie Hoeks <jh@live.com.au>
2014-02-10 09:53:43 +1100

    Added documentation

--- index.js
+++ index.js
@@ -1,24 +1,52 @@
-process.title = 'obvious';
-
-const PERIOD = 500;
-
 var term = require('term');
 
+process.title = 'obvious';
+
+/**
+ * Flash one whole cycle within this period (in milliseconds).
+ *
+ * @const {int}
+ * @default 500
+ */
+const PERIOD = 500;
+
+/**
+ * Takes a string and repeats it a given number of times.
+ *
+ * @param {string} string The string to repeat
+ * @param {int} count Number of times to repeat the string
+ */
 var repeatString = function (string, count) {
 	return new Array(count + 1).join(string);
 };
 
+/**
+ * Fill the terminal buffer with a given color.
+ *
+ * @param {int} w Width of terminal
+ * @param {int} h Height of terminal
+ * @param {int} bg ANSI color to fill screen with
+ * @see http://en.wikipedia.org/wiki/ANSI_escape_code#Colors
+ */
 var drawScreen = function (w, h, bg) {
+	var str = repeatString(' ', w * (h - 1));
 	var output =
-		term.color.bg(
-			repeatString(' ', w * (h - 1)),
-			bg
-		) +
+		term.color.bg(str, bg) +
 		term.cursor.home();
 	return output;
 };
 
+/**
+ * Store the current state of the screen as a boolean. `false` is black and
+ * `true` is white.
+ *
+ * @type {boolean}
+ */
 var currentState = false;
+
+/**
+ * Main loop. This function is executed frequently.
+ */
 var loop = function () {
 	var alt = Date.now() % PERIOD < (PERIOD / 2);
 	if (alt !== currentState) {
@@ -31,23 +59,34 @@ var loop = function () {
 	}
 };
 
+/**
+ * Set up the terminal for main function.
+ */
 var init = function () {
 	var output = term.cursor.hide();
 	process.stdout.write(output);
 };
 
+/**
+ * Clean up the terminal after main function.
+ */
 var finish = function () {
 	var output = term.clear.all() + term.cursor.home();
 	process.stdout.write(output);
 };
 
-init();
-(function inner () {
-	loop();
-	setImmediate(inner);
-})();
+/**
+ * Begin execution.
+ */
+(function () {
+	init();
+	(function inner () {
+		loop();
+		setImmediate(inner);
+	})();
 
-process.on('SIGINT', function () {
-	finish();
-	process.exit();
-});
+	process.on('SIGINT', function () {
+		finish();
+		process.exit();
+	});
+})();
